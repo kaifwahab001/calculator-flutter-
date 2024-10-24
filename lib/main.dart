5593fbbfde4e4a09e692537485665bff541eae25
kaifwahab01 <gandubbb@gmail.com>
2024-10-24 20:42:00 +0530

    calculator initial stage

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'Homepage.dart';
+
+void main (){
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
